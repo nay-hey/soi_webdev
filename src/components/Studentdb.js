f5c59a37051703572456ab4c011655f0accad806
nay-hey <154043375+nay-hey@users.noreply.github.com>
2024-06-22 23:36:18 +0530

    Update Studentdb.js

--- src/components/Studentdb.js
+++ src/components/Studentdb.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState}  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Dropdown, DropdownButton, Badge, Image } from 'react-bootstrap';
 
 import './AdminPage.css';
 import 'simple-datatables/dist/style.css';
@@ -102,7 +103,7 @@ const Studentdb = () => {
 
   return (
     <div>
-        
+            <section id="admin">
       <header id="header" className="header fixed-top d-flex align-items-center">
         <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
             <div className="logo d-flex align-items-center">
@@ -111,197 +112,186 @@ const Studentdb = () => {
             </div>
 
         </div>
-        
-
         <nav className="header-nav ms-auto">
-        <ul className="d-flex align-items-center">
-
-            <li className="nav-item dropdown">
-
-            <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+      <ul className="d-flex align-items-center list-unstyled m-0">
+        <li className="nav-item dropdown me-3">
+          <DropdownButton
+            menuAlign="right"
+            title={
+              <span className="nav-link nav-icon">
                 <i className="bi bi-bell"></i>
-                <span className="badge bg-primary badge-number">4</span>
-            </a>
+                <Badge bg="primary" className="badge-number">
+                  4
+                </Badge>
+              </span>
+            }
+            id="dropdown-notifications"
+          >
+            <Dropdown.Header>You have 4 new notifications</Dropdown.Header>
+            <Dropdown.Divider />
+            <Dropdown.Item className="notification-item">
+              <i className="bi bi-exclamation-circle text-warning"></i>
+              <div>
+                <h4>Lorem Ipsum</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>30 min. ago</p>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="notification-item">
+              <i className="bi bi-x-circle text-danger"></i>
+              <div>
+                <h4>Atque rerum nesciunt</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>1 hr. ago</p>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="notification-item">
+              <i className="bi bi-check-circle text-success"></i>
+              <div>
+                <h4>Sit rerum fuga</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>2 hrs. ago</p>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="notification-item">
+              <i className="bi bi-info-circle text-primary"></i>
+              <div>
+                <h4>Dicta reprehenderit</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>4 hrs. ago</p>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="dropdown-footer">
+              <Link to="#">Show all notifications</Link>
+            </Dropdown.Item>
+          </DropdownButton>
+        </li>
 
-            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
-                <li className="dropdown-header">
-                You have 4 new notifications
-                <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                </li>
-                <li>
-                <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                <i className="bi bi-exclamation-circle text-warning"></i>
-                <div>
-                    <h4>Lorem Ipsum</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>30 min. ago</p>
-                </div>
-                </li>
-
-                <li>
-                <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                <i className="bi bi-x-circle text-danger"></i>
-                <div>
-                    <h4>Atque rerum nesciunt</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>1 hr. ago</p>
-                </div>
-                </li>
-
-                <li>
-                <hr className="dropdown-divider" />
-                </li>
-
-                <li className="notification-item">
-                <i className="bi bi-check-circle text-success"></i>
-                <div>
-                    <h4>Sit rerum fuga</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>2 hrs. ago</p>
-                </div>
-                </li>
-
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li className="notification-item">
-                <i className="bi bi-info-circle text-primary"></i>
-                <div>
-                    <h4>Dicta reprehenderit</h4>
-                    <p>Quae dolorem earum veritatis oditseno</p>
-                    <p>4 hrs. ago</p>
-                </div>
-                </li>
-
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-                <li className="dropdown-footer">
-                <a href="#">Show all notifications</a>
-                </li>
-
-            </ul>
-            </li>
-            <li className="nav-item dropdown">
-
-            <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+        <li className="nav-item dropdown me-3">
+          <DropdownButton
+            menuAlign="right"
+            title={
+              <span className="nav-link nav-icon">
                 <i className="bi bi-chat-left-text"></i>
-                <span className="badge bg-success badge-number">3</span>
-            </a>
+                <Badge bg="success" className="badge-number">
+                  3
+                </Badge>
+              </span>
+            }
+            id="dropdown-messages"
+          >
+            <Dropdown.Header>You have 3 new messages</Dropdown.Header>
+            <Dropdown.Divider />
+            <Dropdown.Item className="message-item">
+              <Link to="#">
+                <Image
+                  src="assets/img/messages-1.jpg"
+                  alt=""
+                  className="rounded-circle me-3"
+                />
+                <div>
+                  <h4>Maria Hudson</h4>
+                  <p>
+                    Velit asperiores et ducimus soluta repudiandae labore
+                    officia est ut...
+                  </p>
+                  <p>4 hrs. ago</p>
+                </div>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="message-item">
+              <Link to="#">
+                <Image
+                  src="assets/img/messages-2.jpg"
+                  alt=""
+                  className="rounded-circle me-3"
+                />
+                <div>
+                  <h4>Anna Nelson</h4>
+                  <p>
+                    Velit asperiores et ducimus soluta repudiandae labore
+                    officia est ut...
+                  </p>
+                  <p>6 hrs. ago</p>
+                </div>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="message-item">
+              <Link to="#">
+                <Image
+                  src="assets/img/messages-3.jpg"
+                  alt=""
+                  className="rounded-circle me-3"
+                />
+                <div>
+                  <h4>David Muldon</h4>
+                  <p>
+                    Velit asperiores et ducimus soluta repudiandae labore
+                    officia est ut...
+                  </p>
+                  <p>8 hrs. ago</p>
+                </div>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="dropdown-footer">
+              <Link to="#">Show all messages</Link>
+            </Dropdown.Item>
+          </DropdownButton>
+        </li>
 
-            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                <li className="dropdown-header">
-                You have 3 new messages
-                <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
-                </li>
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li className="message-item">
-                <a href="#">
-                    <img src="assets/img/messages-1.jpg" alt="" className="rounded-circle" />
-                    <div>
-                    <h4>Maria Hudson</h4>
-                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                    <p>4 hrs. ago</p>
-                    </div>
-                </a>
-                </li>
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li className="message-item">
-                <a href="#">
-                    <img src="assets/img/messages-2.jpg" alt="" className="rounded-circle" />
-                    <div>
-                    <h4>Anna Nelson</h4>
-                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                    <p>6 hrs. ago</p>
-                    </div>
-                </a>
-                </li>
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li className="message-item">
-                <a href="#">
-                    <img src="assets/img/messages-3.jpg" alt="" className="rounded-circle" />
-                    <div>
-                    <h4>David Muldon</h4>
-                    <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                    <p>8 hrs. ago</p>
-                    </div>
-                </a>
-                </li>
-                <li>
-                <hr className="dropdown-divider" />
-                </li>
-
-                <li className="dropdown-footer">
-                <a href="#">Show all messages</a>
-                </li>
-
-            </ul>
-
-            </li>
-
-            <li className="nav-item dropdown pe-3">
-
-            <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                <img src="static/adminpage/profile.png" alt="Profile" className="rounded-circle" />
-                <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
-            </a>
-
-            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                <li className="dropdown-header">
-                <h6>Kevin Anderson</h6>
-                <span>Admin</span>
-                </li>
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li>
-                <Link className="dropdown-item d-flex align-items-center" to="/profile">
-                    <i className="bi bi-person"></i>
-                    <span>My Profile</span>
-                </Link>
-                </li>
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li>
-                <Link className="dropdown-item d-flex align-items-center" to="/profile">
-                    <i className="bi bi-gear"></i>
-                    <span>Account Settings</span>
-                </Link>
-                </li>
-                <li>
-                <hr className="dropdown-divider"/>
-                </li>
-
-                <li>
-                <a className="dropdown-item d-flex align-items-center" href="#">
-                    <i className="bi bi-box-arrow-right"></i>
-                    <span>Sign Out</span>
-                </a>
-                </li>
-
-            </ul>
-            </li>
-        </ul>
-        </nav>
+        <li className="nav-item dropdown">
+          <DropdownButton
+            menuAlign="right"
+            title={
+              <span className="nav-link nav-profile d-flex align-items-center pe-0">
+                <Image
+                  src="static/adminpage/profile.png"
+                  alt="Profile"
+                  className="rounded-circle me-2"
+                />
+                <span className="d-none d-md-block">
+                  K. Anderson
+                </span>
+              </span>
+            }
+            id="dropdown-profile"
+          >
+            <Dropdown.Header>
+              <h6>Kevin Anderson</h6>
+              <span>Admin</span>
+            </Dropdown.Header>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <Link className="dropdown-item d-flex align-items-center" to="/profile">
+                <i className="bi bi-person"></i>
+                <span>My Profile</span>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <Link className="dropdown-item d-flex align-items-center" to="/profile">
+                <i className="bi bi-gear"></i>
+                <span>Account Settings</span>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <Link className="dropdown-item d-flex align-items-center" to="#">
+                <i className="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </Link>
+            </Dropdown.Item>
+          </DropdownButton>
+        </li>
+      </ul>
+    </nav>
            <i className="bi bi-list toggle-sidebar-btn"></i>
         </header>
         <aside id="sidebar" className="sidebar">
@@ -309,13 +299,13 @@ const Studentdb = () => {
             <ul className="sidebar-nav" id="sidebar-nav">
 
             <li className="nav-item">
-                <Link className="nav-link " to="/">
+                <Link className="nav-link collapsed" to="/AdminPage">
                 <i className="bi bi-grid"></i>
                 <span>Home</span>
                 </Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link collapsed" to="/studentdb">
+                <Link className="nav-link " to="/studentdb">
                 <i className="bi bi-layout-text-window-reverse"></i><span>Student Database</span>
                 </Link>
             </li>
@@ -339,6 +329,12 @@ const Studentdb = () => {
                 <span>Profile Edit</span>
                 </Link>
             </li>
+            <li className="nav-item">
+                <Link className="nav-link collapsed" to="/contact">
+                <i class="bi bi-envelope"></i>
+                <span>Contact</span>
+                </Link>
+            </li>
             </ul>
 
             </aside>
@@ -348,7 +344,7 @@ const Studentdb = () => {
             <h1>Student Data</h1>
             <nav>
                 <ol className="breadcrumb">
-                <li className="breadcrumb-item"  style={{ color: "#ccc" }}><Link style={{ color: "#ccc" }} to="/">Home</Link></li>
+                <li className="breadcrumb-item"  style={{ color: "#ccc" }}><Link style={{ color: "#ccc" }} to="/AdminPage">Home</Link></li>
                 <li className="breadcrumb-item active"  style={{ color: "#ccc" }}>Student Database</li>
                 </ol>
             </nav>
@@ -732,6 +728,7 @@ const Studentdb = () => {
             </div>
             </div>
             </footer>
+            </section>
     </div>
   );
 };
