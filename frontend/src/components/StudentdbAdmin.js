6325f396e14e014302300ee5d523371ba5cc60f5
tnvnyk <161897084+tnvnyk@users.noreply.github.com>
2024-06-26 20:00:03 +0530

    Update StudentdbAdmin.js

--- frontend/src/components/StudentdbAdmin.js
+++ frontend/src/components/StudentdbAdmin.js
@@ -7,7 +7,7 @@ import { Dropdown, DropdownButton, Badge, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import './AdminPage.css';
+import './StudentPage.css';
 import 'simple-datatables/dist/style.css';
 import { Tooltip } from 'bootstrap';
 const Studentdb = () => {
@@ -175,157 +175,31 @@ document.addEventListener('scroll', handleScroll);
   return (
     <div>
           <section id="admin">
-      <header id="header" className="header fixed-top d-flex align-items-center">
+          <header id="header" className="header fixed-top d-flex align-items-center">
         <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
             <div className="logo d-flex align-items-center">
-              <img src="static/logo.svg.png" alt="IIT Dharwad Logo" />
+              <img src="/static/logo.svg.png" alt="IIT Dharwad Logo" />
               <h1>IIT Dharwad</h1>
             </div>
-
+            <nav id="navbar" className="navbar">
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/AboutUs/team">Library Committee</a></li>
+                <li><a href="asklib.html">Ask a Librarian</a></li>
+                <li><a href="/AboutUs">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+            </nav>
         </div>
-        
-
         <nav className="header-nav ms-auto">
       <ul className="d-flex align-items-center list-unstyled m-0">
-        <li className="nav-item dropdown me-3">
-          <DropdownButton
-            menuAlign="right"
-            title={
-              <span className="nav-link nav-icon">
-                <i className="bi bi-bell"></i>
-                <Badge bg="primary" className="badge-number">
-                  4
-                </Badge>
-              </span>
-            }
-            id="dropdown-notifications"
-          >
-            <Dropdown.Header>You have 4 new notifications</Dropdown.Header>
-            <Dropdown.Divider />
-            <Dropdown.Item className="notification-item">
-              <i className="bi bi-exclamation-circle text-warning"></i>
-              <div>
-                <h4>Lorem Ipsum</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>30 min. ago</p>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="notification-item">
-              <i className="bi bi-x-circle text-danger"></i>
-              <div>
-                <h4>Atque rerum nesciunt</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>1 hr. ago</p>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="notification-item">
-              <i className="bi bi-check-circle text-success"></i>
-              <div>
-                <h4>Sit rerum fuga</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>2 hrs. ago</p>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="notification-item">
-              <i className="bi bi-info-circle text-primary"></i>
-              <div>
-                <h4>Dicta reprehenderit</h4>
-                <p>Quae dolorem earum veritatis oditseno</p>
-                <p>4 hrs. ago</p>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="dropdown-footer">
-              <Link to="#">Show all notifications</Link>
-            </Dropdown.Item>
-          </DropdownButton>
-        </li>
-
-        <li className="nav-item dropdown me-3">
-          <DropdownButton
-            menuAlign="right"
-            title={
-              <span className="nav-link nav-icon">
-                <i className="bi bi-chat-left-text"></i>
-                <Badge bg="success" className="badge-number">
-                  3
-                </Badge>
-              </span>
-            }
-            id="dropdown-messages"
-          >
-            <Dropdown.Header>You have 3 new messages</Dropdown.Header>
-            <Dropdown.Divider />
-            <Dropdown.Item className="message-item">
-              <Link to="#">
-                <Image
-                  src="assets/img/messages-1.jpg"
-                  alt=""
-                  className="rounded-circle me-3"
-                />
-                <div>
-                  <h4>Maria Hudson</h4>
-                  <p>
-                    Velit asperiores et ducimus soluta repudiandae labore
-                    officia est ut...
-                  </p>
-                  <p>4 hrs. ago</p>
-                </div>
-              </Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="message-item">
-              <Link to="#">
-                <Image
-                  src="assets/img/messages-2.jpg"
-                  alt=""
-                  className="rounded-circle me-3"
-                />
-                <div>
-                  <h4>Anna Nelson</h4>
-                  <p>
-                    Velit asperiores et ducimus soluta repudiandae labore
-                    officia est ut...
-                  </p>
-                  <p>6 hrs. ago</p>
-                </div>
-              </Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="message-item">
-              <Link to="#">
-                <Image
-                  src="assets/img/messages-3.jpg"
-                  alt=""
-                  className="rounded-circle me-3"
-                />
-                <div>
-                  <h4>David Muldon</h4>
-                  <p>
-                    Velit asperiores et ducimus soluta repudiandae labore
-                    officia est ut...
-                  </p>
-                  <p>8 hrs. ago</p>
-                </div>
-              </Link>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className="dropdown-footer">
-              <Link to="#">Show all messages</Link>
-            </Dropdown.Item>
-          </DropdownButton>
-        </li>
-
         <li className="nav-item dropdown">
           <DropdownButton
             menuAlign="right"
             title={
               <span className="nav-link nav-profile d-flex align-items-center pe-0">
                 <Image
-                  src="static/adminpage/profile.png"
+                  src="/static/adminpage/profile.png"
                   alt="Profile"
                   className="rounded-circle me-2"
                 />
@@ -342,21 +216,21 @@ document.addEventListener('scroll', handleScroll);
             </Dropdown.Header>
             <Dropdown.Divider />
             <Dropdown.Item>
-              <Link className="dropdown-item d-flex align-items-center" to="/profile">
+              <Link className="dropdown-item d-flex align-items-center" to="/AdminPage/profile">
                 <i className="bi bi-person"></i>
                 <span>My Profile</span>
               </Link>
             </Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Item>
-              <Link className="dropdown-item d-flex align-items-center" to="/profile">
+              <Link className="dropdown-item d-flex align-items-center" to="/AdminPage/studentprofile">
                 <i className="bi bi-gear"></i>
                 <span>Account Settings</span>
               </Link>
             </Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Item>
-              <Link className="dropdown-item d-flex align-items-center" to="#">
+              <Link className="dropdown-item d-flex align-items-center" to="/Login">
                 <i className="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </Link>
@@ -367,51 +241,60 @@ document.addEventListener('scroll', handleScroll);
     </nav>
            <i className="bi bi-list toggle-sidebar-btn"></i>
         </header>
+
+
         <aside id="sidebar" className="sidebar">
 
-            <ul className="sidebar-nav" id="sidebar-nav">
+<ul className="sidebar-nav" id="sidebar-nav">
 
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="/AdminPage">
-                <i className="bi bi-grid"></i>
-                <span>Home</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="/studentdb">
-                <i className="bi bi-layout-text-window-reverse"></i><span>Student Database</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link " to="/bookdb">
-                <i className="bi bi-book"></i><span>Book Database</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="/circulationmanagement">
-                <i className="bi bi-nut-fill"></i><span>Circulation Management</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="/reminder">
-                <i className="bi bi-alarm-fill"></i><span>Reminder</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="/studentprofile">
-                <i className="bi bi-person"></i>
-                <span>Profile Edit</span>
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="/contact">
-                <i className="bi bi-envelope"></i>
-                <span>Contact</span>
-                </Link>
-            </li>
-            </ul>
+<li className="nav-item">
+    <Link className="nav-link collapsed" to="/AdminPage">
+    <i className="bi bi-grid"></i>
+    <span>Home</span>
+    </Link>
+</li>
+<li className="nav-item">
+    <Link className="nav-link collapsed" to="/AdminPage/studentdb">
+    <i className="bi bi-layout-text-window-reverse"></i><span>Student Database</span>
+    </Link>
+</li>
+<li className="nav-item">
+    <Link className="nav-link collapsed" to="/AdminPage/bookdb">
+    <i className="bi bi-book"></i><span>Book Database</span>
+    </Link>
+</li>
+<li className="nav-item">
+    <Link className="nav-link collapsed" to="/AdminPage/circulationmanagement">
+    <i className="bi bi-nut-fill"></i><span>Circulation Management</span>
+    </Link>
+</li>
+<li className="nav-item">
+    <Link className="nav-link collapsed" to="/AdminPage/reminder">
+    <i className="bi bi-alarm-fill"></i><span>Reminder</span>
+    </Link>
+</li>
+<li className="nav-item">
+    <Link className="nav-link collapsed" to="/AdminPage/studentprofile">
+    <i className="bi bi-person"></i>
+    <span>Profile Edit</span>
+    </Link>
+</li>
+<li className="nav-item">
+        <Link className="nav-link " to="/AdminPage/notification">
+        <i class="bi bi-envelope"></i>
+        <span>Notification</span>
+        </Link>
+    </li>
+</ul>
 
-            </aside>
+</aside>
+
+
+
+
+
+
+        
 
         <main id="main" className="main">
           <div className="pagetitle">
@@ -537,23 +420,109 @@ document.addEventListener('scroll', handleScroll);
         </main>
 
         <footer id="footer" className="footer">
-          <div className="container">
-            <div className="row gy-3">
-              <div className="col-lg-3 col-md-12 footer-info">
-                <div className="logos">
-                  <img src="static/logo.svg.png" alt="Logo" className="logo" />
-                </div>
-                <h3>Indian Institute of Technology Dharwad</h3>
-                <p>Permanent Campus</p>
-                <p>Chikkamalligawad Village</p>
-                <p>Dharwad, Karnataka, India - 580011</p>
-              </div>
-            </div>
-          </div>
-        </footer>
-      </section>
+
+<div className="container">
+<div className="row gy-3">
+    <div className="col-lg-3 col-md-12 footer-info">
+    <div className="logos">
+    <img src="/static/logo.svg.png" alt="Logo" className="logo" />
     </div>
-  );
+    <h3>Indian Institute of Technology Dharwad</h3>
+    <p>Permanent Campus</p>
+    <p>Chikkamalligawad Village</p>
+    <p>Dharwad, Karnataka, India - 580007</p>
+    <p>Email: <a href="mailto:pro@iitdh.ac.in">pro@iitdh.ac.in</a></p>
+    </div>
+
+    <div className="col-lg-2 col-6 footer-links">
+    <h4>Academics</h4>
+        <ul>
+            <li><a href="#">Admissions</a></li>
+            <li><a href="#">Announcements</a></li>
+            <li><a href="#">Departments</a></li>
+            <li><a href="#">Programs</a></li>
+        </ul>
+    </div>
+
+    <div className="col-lg-2 col-6 footer-links">
+    <h4>Research</h4>
+        <ul>
+            <li><a href="#">Consultancy Projects</a></li>
+            <li><a href="#">IRINS</a></li>
+            <li><a href="#">Project Vacancies</a></li>
+            <li><a href="#">Publications</a></li>
+            <li><a href="#">Sponsored Projects</a></li>
+        </ul>
+    </div>
+
+    <div className="col-lg-2 col-6 footer-links">
+    <h4>People</h4>
+        <ul>
+            <li><a href="#">Administration</a></li>
+            <li><a href="#">Faculty</a></li>
+            <li><a href="#">Staff</a></li>
+            <li><a href="#">Students</a></li>
+        </ul>
+    </div>
+
+    <div className="col-lg-2 col-6 footer-links">
+    <h4>Quick Access</h4>
+        <ul>
+        <li><a href="#">About Dharwad</a></li>
+            <li><a href="#">Bus Schedule</a></li>
+            <li><a href="#">Chief Vigilance Officer</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Counselling Center</a></li>
+            <li><a href="#">CSR</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Grievance Redressal</a></li>
+            <li><a href="#">ICC</a></li>
+            <li><a href="#">Intranet</a></li>
+            <li><a href="#">Old Website</a></li>
+            <li><a href="#">RTI</a></li>
+            <li><a href="#">SC-ST-OBC Liaison Cell</a></li>
+            <li><a href="#">Tenders</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">VPN Access</a></li>
+        </ul>
+    </div>
+
+</div>
+</div>
+
+<div className="footer-legal">
+<div className="container">
+
+    <div className="row justify-content-between">
+    <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+        <div className="copyright">
+        © Copyright <strong><span>IIT Dharwad</span></strong>. All Rights Reserved
+        </div>
+
+        <div className="credits">
+        Designed by Pandas🐼
+        </div>
+
+    </div>
+
+    <div className="col-md-6">
+        <div className="social-links mb-3 mb-lg-0 text-center text-md-end">
+        <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
+        <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
+        <a href="#" className="instagram"><i className="bi bi-instagram"></i></a>
+        <a href="#" className="linkedin"><i className="bi bi-linkedin"></i></a>
+        </div>
+
+    </div>
+
+    </div>
+
+</div>
+</div>
+</footer>
+</section>
+</div>
+);
 };
 
 export default Studentdb;
