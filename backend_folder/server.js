13c2361ae267b38a66fc84829fd132ae58c4ccb0
tnvnyk <161897084+tnvnyk@users.noreply.github.com>
2024-06-26 16:27:33 +0530

    Update server.js - studentdb admin page

--- backend_folder/server.js
+++ backend_folder/server.js
@@ -18,6 +18,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 connectDB();
 
 // Routes setup
+const studentRoutes =require('./routes/studentRoutes');//for admin page- studentdb, edit made on 26th june
 const bookRoutes = require('./routes/bookRoutes');
 const issueRoutes = require('./routes/issueRoutes'); // Import issue routes
 const sendReminderRoutes = require('./routes/sendReminderRoutes'); 
@@ -27,6 +28,7 @@ app.use('/api/books', bookRoutes);
 app.use('/api/issues', issueRoutes); // Mount issue routes
 app.use('/api', sendReminderRoutes);
 app.use('/api/notification', notification);
+app.use('/api/students', studentRoutes); //for admin page- studentdb, edit made on 26th june
 
 // Start server
 app.listen(port, () => {
