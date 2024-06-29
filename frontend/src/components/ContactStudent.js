e93fc98d8fb82fffcf8e5376ef588c4ddbf33230
nay-hey <154043375+nay-hey@users.noreply.github.com>
2024-06-30 01:30:57 +0530

    Update ContactStudent.js

--- frontend/src/components/ContactStudent.js
+++ frontend/src/components/ContactStudent.js
@@ -114,15 +114,14 @@ const Contactdb = () => {
                         className="rounded-circle me-2"
                       />
                       <span className="d-none d-md-block">
-                        K. Anderson
+                        User
                       </span>
                     </span>
                   }
                   id="dropdown-profile"
                 >
                   <Dropdown.Header>
-                    <h6>Kevin Anderson</h6>
-                    <span>Admin</span>
+                    <h6>User</h6>
                   </Dropdown.Header>
                   <Dropdown.Divider />
                   <Dropdown.Item>
